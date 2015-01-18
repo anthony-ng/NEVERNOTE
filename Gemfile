39e3a4ea986bba14ec5da2aa2916e447b7565d1d
anthony-ng <thomsonng123@gmail.com>
2015-01-17 16:57:13 -0800

    update environment

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 ruby '2.0.0'
 
+gem 'shotgun'
+
 # PostgreSQL driver
 gem 'pg'
 
--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,7 +1,43 @@
 get '/' do
-  # Look in app/views/index.erb
- 
-  @readme_content = File.read(APP_ROOT.join('README.md'))
+
 
   erb :index
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # @readme_content = File.read(APP_ROOT.join('README.md'))
